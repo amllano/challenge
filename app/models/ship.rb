ae3be124d4d3db7e864bbe341ff7fbf5ec263976
Martin Llano <amllano@outlook.com>
2016-09-02 10:59:14 -0300

    Implement method ship#is_at to check is a ship is in a given position

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,3 +1,12 @@
 class Ship < ApplicationRecord
   belongs_to :game
+
+  # Returns true if ships is at the given position.
+  def is_at(x, y)
+	if (self.orientation == "horizontal") 
+		return (y === self.origin_y && x.between?(self.origin_x, self.origin_x + (self.size - 1)))
+	end
+		
+	(x === self.origin_x && y.between?(self.origin_y, self.origin_y + (self.size - 1)))
+  end
 end
--- spec/models/ship_spec.rb
+++ spec/models/ship_spec.rb
@@ -1,5 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe Ship, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#is_at" do
+		it "returns true when ships is at coordinates x, y" do
+			# @Todo: Use before hooks and factories to DRY.
+			horizontal_carrier = Ship.create(:name => "Aircraft carrier", :origin_x=> 1, :origin_y => 1, :size => 5, :orientation => 'horizontal')
+			expect(horizontal_carrier.is_at(1, 1)).to eql(true)
+			expect(horizontal_carrier.is_at(2, 1)).to eql(true)
+			expect(horizontal_carrier.is_at(3, 1)).to eql(true)
+			expect(horizontal_carrier.is_at(4, 1)).to eql(true)
+			expect(horizontal_carrier.is_at(5, 1)).to eql(true)
+
+			vertical_carrier = Ship.create(:name => "Aircraft carrier", :origin_x=> 1, :origin_y => 1, :size => 5, :orientation => 'vertical')
+			expect(vertical_carrier.is_at(1, 1)).to eql(true)
+			expect(vertical_carrier.is_at(1, 2)).to eql(true)
+			expect(vertical_carrier.is_at(1, 3)).to eql(true)
+			expect(vertical_carrier.is_at(1, 4)).to eql(true)
+			expect(vertical_carrier.is_at(1, 5)).to eql(true)
+		end
+
+		it "returns false when ships is not at coordinates x, y" do
+			# @Todo: Use before hooks and factories to DRY.			
+			horizontal_carrier = Ship.create(:name => "Aircraft carrier", :origin_x=> 1, :origin_y => 1, :size => 5, :orientation => 'horizontal')
+			expect(horizontal_carrier.is_at(1, 2)).to eql(false)
+			expect(horizontal_carrier.is_at(2, 2)).to eql(false)
+			expect(horizontal_carrier.is_at(3, 2)).to eql(false)
+			expect(horizontal_carrier.is_at(4, 2)).to eql(false)
+			expect(horizontal_carrier.is_at(5, 2)).to eql(false)
+			expect(horizontal_carrier.is_at(6, 2)).to eql(false)
+			expect(horizontal_carrier.is_at(6, 1)).to eql(false)
+
+			vertical_carrier = Ship.create(:name => "Aircraft carrier", :origin_x=> 1, :origin_y => 1, :size => 5, :orientation => 'vertical')
+			expect(vertical_carrier.is_at(2, 1)).to eql(false)
+			expect(vertical_carrier.is_at(2, 2)).to eql(false)
+			expect(vertical_carrier.is_at(2, 3)).to eql(false)
+			expect(vertical_carrier.is_at(2, 4)).to eql(false)
+			expect(vertical_carrier.is_at(2, 5)).to eql(false)
+			expect(vertical_carrier.is_at(2, 6)).to eql(false)
+			expect(vertical_carrier.is_at(1, 6)).to eql(false)
+		end
+	end 
 end
