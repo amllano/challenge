9c4c02ddd5e986d6341b6418ba571549de58a2f4
Martin Llano <amllano@outlook.com>
2016-09-02 12:13:04 -0300

    Add validation for guess model

--- app/models/guess.rb
+++ app/models/guess.rb
@@ -1,3 +1,5 @@
 class Guess < ApplicationRecord
   belongs_to :game
+  validates :x, presence: true, :inclusion => {:in => 1..10}
+  validates :y, presence: true, :inclusion => {:in => 1..10}                        
 end
