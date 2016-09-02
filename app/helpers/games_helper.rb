f0921a9b4a4934f16c38a10c599fef529d699551
Martin Llano <amllano@outlook.com>
2016-09-02 11:35:11 -0300

    Display own ships on game show view

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -3,4 +3,34 @@ module GamesHelper
 	def player_turn_text(turn)
 		turn ? "Your Turn" : "Waiting for Opponent"
 	end
+
+	# Returns ship name if there is a ship at the given coordinate (x, y); nil in other case
+	def find_ship(x, y, ships)
+		_has_ship = false
+		ships.each do |ship|
+			_has_ship = ship.is_at(x, y)
+
+			if (_has_ship) 
+				return ship
+			end
+		end
+
+		nil		
+	end
+
+	# Maps ship name (type) to background color class
+	def ship_name_bg_class(name) 
+		case name
+		when "Aircraft carrier"
+			return "bg bg-primary"
+		when "Battleship"
+			return "bg bg-success"
+		when "Submarine"
+			return "bg bg-warning"
+		when "Destroyer"
+			return "bg bg-info"
+		when "Patrol Boat"
+			return "bg bg-danger"
+		end
+	end
 end
--- spec/helpers/games_helper_spec.rb
+++ spec/helpers/games_helper_spec.rb
@@ -11,5 +11,26 @@ require 'rails_helper'
 #   end
 # end
 RSpec.describe GamesHelper, :type => :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#find_ship" do
+		it "returns ship given coordinates" do
+			fleet = [
+			 Ship.create(:name => "Aircraft carrier", :origin_x=> 1, :origin_y => 1, :size => 5, :orientation => 'horizontal'),
+			 Ship.create(:name => "Battleship", :origin_x=> 1, :origin_y => 2, :size => 4, :orientation => 'horizontal'),
+			 Ship.create(:name => "Submarine", :origin_x=> 5, :origin_y => 5, :size => 3, :orientation => 'vertical'),
+			]
+
+			expect(helper.find_ship(1, 1, fleet).name).to eql("Aircraft carrier")
+			expect(helper.find_ship(5, 1, fleet).name).to eql("Aircraft carrier")
+			expect(helper.find_ship(6, 1, fleet)).to eql(nil)
+
+			expect(helper.find_ship(1, 2, fleet).name).to eql("Battleship")
+			expect(helper.find_ship(4, 2, fleet).name).to eql("Battleship")
+			expect(helper.find_ship(5, 2, fleet)).to eql(nil)
+
+			expect(helper.find_ship(5, 5, fleet).name).to eql("Submarine")
+			expect(helper.find_ship(5, 7, fleet).name).to eql("Submarine")
+			expect(helper.find_ship(5, 4, fleet)).to eql(nil)
+			expect(helper.find_ship(5, 8, fleet)).to eql(nil)
+		end
+	end	
 end
