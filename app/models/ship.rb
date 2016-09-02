d4290a6729c2ad928b905b1e93f752d66e65b29c
Martin Llano <amllano@outlook.com>
2016-09-02 12:12:03 -0300

    Update ship status given guesses

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -9,4 +9,18 @@ class Ship < ApplicationRecord
 		
 	(x === self.origin_x && y.between?(self.origin_y, self.origin_y + (self.size - 1)))
   end
+
+  #Updates ships status given opponent guesses
+  def update_status(guesses) 
+  	_count = 0
+  	guesses.each do |guess|
+  		if (guess.status == "hit") && (self.is_at(guess.x, guess.y))
+  			_count += 1
+  		end
+  	end
+
+  	self.status = _count === self.size ? "sunk" : "safe"
+
+  	self.status
+  end
 end
--- spec/models/ship_spec.rb
+++ spec/models/ship_spec.rb
@@ -39,5 +39,36 @@ RSpec.describe Ship, :type => :model do
 			expect(vertical_carrier.is_at(2, 6)).to eql(false)
 			expect(vertical_carrier.is_at(1, 6)).to eql(false)
 		end
-	end 
+	end
+
+	describe "#update_status" do
+		it "returns sunk when a ship has been hit in all her extension" do		
+			guessesOne = [
+			  Guess.create(:x => 1,  :y => 1, :status => "hit"),
+			  Guess.create(:x => 2,  :y => 1, :status => "hit"),
+			  Guess.create(:x => 3,  :y => 1, :status => "hit")
+			]
+
+			guessesTwo = [
+			  Guess.create(:x => 1,  :y => 1, :status => "hit"),
+			  Guess.create(:x => 1,  :y => 2, :status => "hit"),
+			  Guess.create(:x => 1,  :y => 3, :status => "hit")
+			]
+
+			submarine_horizontal = Ship.create(:name => "Submarine", :origin_x=> 1, :origin_y => 1, :size => 3, :orientation => 'horizontal', :status => "safe")
+			submarine_vertical = Ship.create(:name => "Submarine", :origin_x=> 1, :origin_y => 1, :size => 3, :orientation => 'vertical', :status => "safe")
+
+			submarine_horizontal.update_status(guessesOne)			
+			expect(submarine_horizontal.status).to eql('sunk')
+
+			submarine_horizontal.update_status(guessesTwo)			
+			expect(submarine_horizontal.status).to eql('safe')
+
+			submarine_vertical.update_status(guessesTwo)			
+			expect(submarine_vertical.status).to eql('sunk')
+			
+			submarine_vertical.update_status(guessesOne)			
+			expect(submarine_vertical.status).to eql('safe')
+		end
+	end
 end
