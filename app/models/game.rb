af1932d16a574cce28ed66492b97086fe174dfe7
Martin Llano <amllano@outlook.com>
2016-09-02 12:45:03 -0300

    Calculate game status given opponent guesses

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,4 +2,19 @@ class Game < ApplicationRecord
   has_one :opponent, class_name: "Game", foreign_key: "opponent_id"
   has_many :ships
   has_many :guesses  
+
+  # calculate game status
+  def update_status (guesses)
+ 		game_over = true
+ 		self.ships do |ship|
+ 			ship.update_status(guesses)
+ 			ship.save
+ 			
+ 			game_over &&= ship.status == "sunk"
+ 		end
+
+ 		if (game_over) 
+ 			self.status = "game over"
+ 		end
+ 	end
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Game, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#update_status" do
+		it "returns 'game over' when all player's ships has been sunk" do
+			_playerOne = Game.create(id: 1, player: '1', status:'playing', turn: true, opponent_id: 2)			
+			Ship.create([
+			{ name: 'Submarine', 
+				status: 'safe', 
+				size: 1, 
+				origin_x: 1, 
+				origin_y: 1, 
+				orientation: 'horizontal',
+				game: _playerOne 
+			}, 
+			{ 
+				name: 'Submarine', 
+				status: 'safe', 
+				size: 1, 
+				origin_x: 2, 
+				origin_y: 2, 
+				orientation: 'horizontal',
+				game: _playerOne 
+			}])		
+
+			_guesses = [
+			  Guess.create(:x => 1,  :y => 1, :status => "hit"),
+			  Guess.create(:x => 2,  :y => 2, :status => "hit"),
+			]
+
+			_playerOne.update_status(_guesses)
+			expect(_playerOne.status).to eql('game over')
+		end
+	end
 end
